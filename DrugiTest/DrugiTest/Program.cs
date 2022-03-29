6bb289d76a5296e99fc0cac78c0caf4fcdfcb396
Nik Kranjc Gregoric <nik.kranjc.gregoric@gmail.com>
2022-03-29 09:19:04 +0200

    ponovitev linq

--- DrugiTest/DrugiTest/Program.cs
+++ DrugiTest/DrugiTest/Program.cs
@@ -10,7 +10,15 @@ namespace DrugiTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello git from console");
+            BazaZaVajeEntities context = new BazaZaVajeEntities();
+            //vsi dobavitelji
+            //var x1 = from a in context.DOBAVITELJ
+            //         select a;
+            var x1 = context.DOBAVITELJ;
+            foreach(var y in x1)
+            {
+                Console.WriteLine(y.D_IME + " " + y.D_KONTAKT);
+            }
             Console.ReadLine();
         }
     }
